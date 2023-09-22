471251eed9d68792f74b34c78f0c6bf622c9464a
Ismail Ananouche <123566796+Ismail-3-an@users.noreply.github.com>
2023-09-22 17:23:13 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     <footer className='bg-gray-200 mt-11 flex items-center flex-col p-3 shadow-lg text-gray-600'>
       <h1 className='mt-2 font-semibold italic'>© 2023 Cara. All rights are reserved</h1>
       <h1 className='font-bold mt-3'>Secured Payments Gateways:</h1>
-      <Image src={jpg} className='mt-3'/>
+      <Image src={jpg} alt="sjsj" className='mt-3'/>
       <div className= 'flex items-center cursor-pointer mt-3 gap-3 underline font-semibold'>
         <p className=''>Security</p>
         <p>Contact us</p>
